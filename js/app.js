3a9cf9c2f6734309ddf3a02dac21e8aa01892b30
koheinthurako <captainthura99@gmail.com>
2023-06-06 20:03:20 +0630

    Finished JS mini project

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const items = document.getElementById('items');
+const inputForm = document.getElementById('inputForm');
+const quantity = document.getElementById('quantity');
+const rows = document.getElementById('rows');
+const total = document.getElementById('total');
+
+
+function calTotalCost() {
+    let costs = document.querySelectorAll(".cost");
+    total.innerText = [...costs].reduce((pv, cv) => pv + Number(cv.innerText) , 0)
+}
+
+function delItem(event) {
+    if(confirm("Are you sure to delete this book?")) {
+        event.target.parentElement.parentElement.remove();
+        calTotalCost();
+    }
+}
+
+products.forEach(function(product) {
+    let newOption = new Option(product.name, product.id);
+    items.append(newOption);
+});
+
+
+inputForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let currentItem = products.find(product => product.id == items.value);
+    let cost = currentItem.price * quantity.valueAsNumber;
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+                <td>
+                    ${currentItem.name}
+                    <span class="text-danger small delBtn" onclick="delItem(event)">Delete</span>
+                </td>
+                <td class="text-end">${currentItem.price}</td>
+                <td class="text-end">${quantity.value}</td>
+                <td class="text-end cost">${cost}</td>
+                `;cost
+    rows.append(tr);
+    calTotalCost();
+    inputForm.reset();
+})
+
---
+++ js/products.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        id : 1,
+        name : "Minnanonihongo N5",
+        price : 5000
+    }, 
+    {
+        id : 2,
+        name : "Kanji Challenge N5",
+        price : 3000
+    }, 
+    {
+        id : 3,
+        name : "Minnanonihongo N4",
+        price : 6000
+    }, 
+    {
+        id : 4,
+        name : "Kanji Challenge N4",
+        price : 3000
+    }, 
+    {
+        id : 5,
+        name : "Somatome N4",
+        price : 5000
+    }
+]
